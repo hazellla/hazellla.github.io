2157c21ea86c3072b45f43021dcf4749e1548c71
hazellla <hazel1997623@gmail.com>
2023-02-28 17:41:17 -0500

    update1

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -90,6 +90,20 @@ const config = {
       imageUrl: 'https://via.placeholder.com/250x250',
       link: 'https://example.com',
     },
+    {
+      title: 'Project Name',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+      imageUrl: 'https://via.placeholder.com/250x250',
+      link: 'https://example.com',
+    },
+    {
+      title: 'Project Name',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+      imageUrl: 'https://via.placeholder.com/250x250',
+      link: 'https://example.com',
+    },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
